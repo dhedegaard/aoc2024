1f91c759bd43211de359137d0ca7d6dbc97ebf08
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-11-25 21:16:57 +0100

    day01 part 2

--- day01.ts
+++ day01.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+console.time("parse");
 const stdin = await Bun.stdin.text();
 const lines = stdin.split("\n");
 const [firstList, lastList] = lines.reduce<[number[], number[]]>(
@@ -16,14 +17,25 @@ const [firstList, lastList] = lines.reduce<[number[], number[]]>(
   },
   [[], []]
 );
+console.timeEnd("parse");
 
 {
   console.time("part1");
   let result = 0;
   for (let i = 0; i < firstList.length; i++) {
-    result += Math.abs(
-      z.int().parse(firstList[i]) - z.int().parse(lastList[i])
-    );
+    result += Math.abs((firstList[i] as number) - (lastList[i] as number));
   }
   console.timeLog("part1", result);
+}
+
+{
+  console.time("part2");
+  const countedLastList = lastList.reduce<Map<number, number>>((acc, value) => {
+    acc.set(value, (acc.get(value) ?? 0) + 1);
+    return acc;
+  }, new Map<number, number>());
+  const result = firstList
+    .map((number) => (countedLastList.get(number) ?? 0) * number)
+    .reduce((acc, result) => acc + result, 0);
+  console.timeLog("part2", result);
 }
